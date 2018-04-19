cdd8e49858cb2c835e8149da1bf385bb5b84114d
Dong Park <dpark032@hammer.cs.ucr.edu>
2018-04-19 16:09:27 -0700

    added list and vector classes

--- container.h
+++ container.h
@@ -1,6 +1,14 @@
 #ifndef __CONTAINER_H__
 #define __CONTAINER_H__
 
+#include <iostream>
+#include <vector>
+#include <iterator>
+
+#include "strategy.h"
+
+using namespace std;
+
 class Container {
     protected: 
         Sort* sort_function;
@@ -12,7 +20,9 @@ class Container {
 
         /* Non Virtual Functions */
         // Set the type of sorting algorithm
-        void set_sort_function(Sort* sort_function); 
+        void set_sort_function(Sort* sort_function) {
+		this->sort_function = sort_function;
+	}; 
 
         /* Pure Virtual Functions */
         // insert the top pointer of the tree into the container
@@ -31,5 +41,85 @@ class Container {
         // return container size;
         virtual int size() = 0;
 };
+
+class listContainer: public Container {
+	private:
+		list<Base*> numList;
+
+	public:
+		void add_element(Base* element) {
+			numList.push_back(element);
+		};
+		Base* at(int i) {
+			list<Base*>::iterator it = numList.begin();
+			advance(it, i);
+			return *it;
+		};
+		void print() {
+			for(list<Base*>::iterator i = numList.begin(); i != numList.end(); ++i) {
+				cout << *i << " ";
+			}
+		};
+		void sort() {
+			try {
+				if(sort_function == NULL) {
+					throw 1;
+				}
+			}
+			catch(int error_val) {
+				cout << "Error: Sort function not selected." << endl;
+			}
+			sort_function->sort(this);
+		};
+		void swap(int i, int j) {
+			list<Base*>::iterator first = numList.begin();
+			list<Base*>::iterator second = numList.begin();
+			advance(first, i);
+			advance(second, j);
+			swap(*first, *second);
+		};
+		int size() {
+			return numList.size();
+		};
+};
+
+class VectorContainer: public Container {
+        private:
+                vector<Base*> numVec;
+
+        public:
+                void add_element(Base* element) {
+                        numVec.push_back(element);
+                };
+		Base* at(int i) {
+			return numVec.at(i);
+		};
+                void print() {
+                        vector<Base*>::iterator it;
+			for(it = numVec.begin(); it != vec.end(); ++it) {
+				cout << *it << " ";
+			}
+                };
+                void sort() {
+                        try {
+                                if(sort_function == NULL) {
+                                        throw 1;
+                                }
+                        }
+                        catch(int error_val) {
+                                cout << "Error: Sort function not selected." << endl;
+                        }
+                        sort_function->sort(this);
+                };
+                void swap(int i, int j) {
+                        Base* temp = numVec.at(i);
+			numVec.at(i) = numVec.at(j);
+			numVec.at(j) = temp;
+                };
+                int size() {
+                        return numVec.size();
+                };
+};
+
 
 #endif // __CONTAINER_H__
