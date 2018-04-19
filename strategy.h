d2e379d6b294b8bbb72f7d250c52541528e84e42
Dong Park <dpark032@hammer.cs.ucr.edu>
2018-04-19 16:23:16 -0700

    Added sorting functions

--- strategy.h
+++ strategy.h
@@ -1,6 +1,7 @@
 #ifndef __STRATEGY_H__
 #define __STRATEGY_H__
 
+class Container;
 class Sort {
     public: 
         /* Constructors */
@@ -8,6 +9,40 @@ class Sort {
 
         /* Pure Virtual Functions */
         virtual void sort(Container* container) = 0;
+};
+
+class SelectionSort: public Sort {
+	public:
+		SelectionSort() : Sort() { };
+		void sort(Container* container) {
+			for(int i = 0; i < container->size() - 1; i++) {
+				int min = i;
+				for(int j = i + 1; j < container->size(); j++) {
+					if(container->at(j) < container->at(min)) {
+						min = j;
+					}
+					Base* temp = container->at(min);
+					*container->at(min) = *container->at(i);
+					*container->at(i) = *temp;
+				}
+			}
+		};
+};
+
+class BubbleSort: public Sort {
+	public:
+		BubbleSort() : Sort() { };
+		void sort(Container* container) {
+			for(int i = 0; i < container->size() - 1; i++) {
+				for(int j = 0; j < container->size() - i - 1; i++) {
+					if(container->at(j) > container->at(j + 1)) {
+						Base* temp = container->at(j + 1);
+						*container->at(j) = *(container->(j + 1));
+						*container->at(j + 1) = *temp;
+					}
+				}
+			}
+		};
 };
 
 #endif // __STRATEGY_H__
