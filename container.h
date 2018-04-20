f7057301063b019e39972f63271b4e447c5eaa64
Dong Park <dpark032@hammer.cs.ucr.edu>
2018-04-19 20:49:38 -0700

    Updated

--- container.h
+++ container.h
@@ -2,10 +2,12 @@
 #define __CONTAINER_H__
 
 #include <iostream>
+#include <cstdlib>
 #include <vector>
 #include <iterator>
 
 #include "strategy.h"
+#include "component.h"
 
 using namespace std;
 
@@ -15,7 +17,7 @@ class Container {
 
     public: 
         /* Cosntructors */
-        Container() : sort_function(NULL) { }
+        Container() : sort_function(NULL) { };
         Container(Sort* function); 
 
         /* Non Virtual Functions */
@@ -40,85 +42,6 @@ class Container {
         virtual Base* at(int i) = 0;
         // return container size;
         virtual int size() = 0;
-};
-
-class listContainer: public Container {
-	private:
-		list<Base*> numList;
-
-	public:
-		void add_element(Base* element) {
-			numList.push_back(element);
-		};
-		Base* at(int i) {
-			list<Base*>::iterator it = numList.begin();
-			advance(it, i);
-			return *it;
-		};
-		void print() {
-			for(list<Base*>::iterator i = numList.begin(); i != numList.end(); ++i) {
-				cout << *i << " ";
-			}
-		};
-		void sort() {
-			try {
-				if(sort_function == NULL) {
-					throw 1;
-				}
-			}
-			catch(int error_val) {
-				cout << "Error: Sort function not selected." << endl;
-			}
-			sort_function->sort(this);
-		};
-		void swap(int i, int j) {
-			list<Base*>::iterator first = numList.begin();
-			list<Base*>::iterator second = numList.begin();
-			advance(first, i);
-			advance(second, j);
-			swap(*first, *second);
-		};
-		int size() {
-			return numList.size();
-		};
-};
-
-class VectorContainer: public Container {
-        private:
-                vector<Base*> numVec;
-
-        public:
-                void add_element(Base* element) {
-                        numVec.push_back(element);
-                };
-		Base* at(int i) {
-			return numVec.at(i);
-		};
-                void print() {
-                        vector<Base*>::iterator it;
-			for(it = numVec.begin(); it != vec.end(); ++it) {
-				cout << *it << " ";
-			}
-                };
-                void sort() {
-                        try {
-                                if(sort_function == NULL) {
-                                        throw 1;
-                                }
-                        }
-                        catch(int error_val) {
-                                cout << "Error: Sort function not selected." << endl;
-                        }
-                        sort_function->sort(this);
-                };
-                void swap(int i, int j) {
-                        Base* temp = numVec.at(i);
-			numVec.at(i) = numVec.at(j);
-			numVec.at(j) = temp;
-                };
-                int size() {
-                        return numVec.size();
-                };
 };
 
 
